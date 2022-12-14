48db1516464bd987696b893b9f734cb1094cd653
COdestriker05 <114793925+COdestriker05@users.noreply.github.com>
2022-12-14 19:15:34 +0530

    Add files via upload

---
+++ class7/class7-Update_dict-CipherSchools.py
@@ -0,0 +1,11 @@
+user_info = {
+    'name' : 'Shivam',
+    'age' : 19,
+    'fav_movies' : ['coco', 'kimi no na wa'],
+    'fav_tunes' : ['awakening', 'fairy tale']
+}
+
+more_info = {'State' : 'Haryana', 'Hobbies' : ['Coding','Reading','Guitar']}
+
+user_info.update(more_info)
+print(user_info)
